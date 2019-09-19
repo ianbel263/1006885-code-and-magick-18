ec5f7b760c815abea047cdda3a493bed452c6185
Andrey Belyaev <ianbel263@gmail.com>
2019-09-19 12:09:17 +0300

    Убирает округление min, max в генераторе случайных чисел

--- js/setup.js
+++ js/setup.js
@@ -16,8 +16,6 @@ var MOCK = {
 var NUMBER_OF_WIZARDS = 4;
 
 var getRandomInt = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
